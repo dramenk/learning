4088d49c1a3473d1b301e12a1852ce8cb36be022
kelongwen <kelongwen@localhost.localdomain>
2015-06-28 23:22:24 +0800

    3-->表达式

--- script/lua/3learningLua.lua
+++ script/lua/3learningLua.lua
@@ -1,8 +1,22 @@
 #!/usr/local/bin/lua
 
-print("This script is learning for table in LUA.")
+--[[
+-- 表达式
+--]]--
+
+-- 1. 算术运算符：略
+-- 2. 关系运算符：不等号为～=
+-- 3. 逻辑运算符：and or not
+a = nil; b = 2
+print("a(false) and b(2) is ", (a and b)) 	-->a is false, a and b return a 
+print("a(false) or b(2) is ", (a or b)) 	-->a is false, a or b return b 
+
+a = 1; b = 2 
+print("a(1) and b(2) is ", (a and b))		-->a is true, a and b returns b
+print("a(1) or b(2) is ", (a or b))			-->a is true, a and b returns a
 
 -- table在其他语言中称作map映射或者dict字典
+-- 下面是table的构造器
 theTable = {
 	x = "1", 
 	y = 2,
@@ -10,7 +24,7 @@ theTable = {
 }
 
 -- 可以新增filed
-theTable["newFiled1"] = 60
+theTable["Filed1"] = 60
 theTable[2] = 30
 
 -- 通过filed的key获取value
@@ -25,3 +39,8 @@ theTable[1]=nil
 
 -- 下标为1的元素已经被删除，下标2的元素也不会被提前，这里元素2依然是30
 print('theTable[2] is ' .. theTable[2])
+
+-- 这里的迭代是无序的
+for k, v in pairs(theTable) do 
+	io.write(k..'\t'..v..'\n')
+end
