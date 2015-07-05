81e22ee2c142fbd5e68b42562c1523f1ce9196ab
kelongwen <kelongwen@localhost.localdomain>
2015-07-06 02:10:55 +0800

    新增：函数，切片，迭代，列表生成式

--- script/python/python3/2.py
+++ script/python/python3/2.py
@@ -1,2 +1,44 @@
 #!/usr/bin/python3.4
 
+# 定义函数
+def my_abs(x):
+	# 参数个数检查可以自动完成，但是参数类型检查需要这样：
+	if not isinstance(x, (int, float)):
+		raise TypeError('bad operand type')
+	if x > 0:
+		return x
+	else:
+		return -x
+
+# 空函数
+def nop():
+	pass
+
+# 函数本身也是第一类值
+a = my_abs
+
+# 函数调用
+print("my_abs(-6)", a(-6))
+
+# 支持默认参数
+# 必选参数在前，默认参数在后，否则Python的解释器会报错；
+# 当函数有多个参数时，把变化大的参数放前面，变化小的参数放后面。变化小
+# 的参数就可以作为默认参数。只有与默认参数不符时才需要提供额外的信息。
+def power(x, n=2):
+	s = 1
+	while n > 0:
+		n = n - 1
+		s = s * x
+	return s
+print("power(6)", power(6))
+
+# 支持可变参数，参数个数可变
+def calc(*numbers):
+	sum = 0
+	for n in numbers:
+		sum = sum + n * n
+	return sum
+print(calc(1, 2, 3, 4))
+
+
+
--- script/python/python3/3.py
+++ script/python/python3/3.py
@@ -1,2 +1,31 @@
 #!/usr/bin/python3.4
+import collections
+import os
+
+# 切片
+L = ['Michael', 'Sarah', 'Tracy', 'Bob', 'Jack']
+print("L = ['Michael', 'Sarah', 'Tracy', 'Bob', 'Jack']")
+
+print(L[0:3])	# 0到3但不包括3
+print(L[:3])	# 0可以省略
+print(L[0:3:2])	# 步长为2
+
+print(L[-2:])	# 从后往前
+print(L[-2:-1]) # 从-2到-1（最后一个），但是不包括-1
+
+print((1, 2, 3, 4, 5)[0:3])	# tuple能切片
+print('ABCDEF'[0:3])		# 字符串能切片
+
+# 迭代
+print('"ABCD"可迭代？', isinstance('ABCD', collections.Iterable)) # 判断是否可以迭代
+d = {'a': 1, 'b': 2, 'c': 3}
+for k in d:
+	print(k)
+
+# 列表生成式
+a = [x * x for x in range(11)]
+print(a)
+a = [x * x for x in range(11) if x % 2 == 0] # 还可以增加限制条件
+print(a)
+print([d for d in os.listdir('.')])
 
