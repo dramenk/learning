445c7d1bf5acf00e1e69f89a672032b03a7b690c
kelongwen <kelongwen@localhost.localdomain>
2015-07-06 00:15:16 +0800

    目录结构变化

--- script/python/five.py
+++
@@ -1,7 +0,0 @@
-class myObject(object):
-	
-	def __score__(self):
-		return 100
-
-obj = myObject()
-print score(obj)
--- script/python/vodprint/setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup
-
-setup(
-		name = 'vodprint',
-		version = '1.0.0',
-		py_modules = ['vodprint']
-	 )
