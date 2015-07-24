5f176fcfb481c51bebaf2f982ea556bb7fed43f2
kelongwen <kelongwen@localhost.localdomain>
2015-07-25 00:42:10 +0800

    补充：bash shell，读取命令输出

--- script/shell/05_time.sh
+++ script/shell/05_time.sh
@@ -2,7 +2,7 @@
 
 # 日期/时间/延时
 
-begin=`date +%s`
+begin=`date +%s`									# 读取命令序列的输出，还有$(...)
 date +%s											# Posix时间戳，1970.01.01 00:00
 date --date "Thu Jul 23 02:17:59 CST 2015" +%s		# --date表示提供日期串作为输入
 date "+%Y %B %d"									# 格式化：年月日
